c8e12fd360aa52131df27b5f2e11b17c491fa014
samrio_1122 <5260a.rio@gmail.com>
2021-08-08 20:41:49 +0900

    ストーリーボードとデザインライブラリ

--- Pods/Target Support Files/Pods-match/Pods-match-frameworks.sh
+++ Pods/Target Support Files/Pods-match/Pods-match-frameworks.sh
@@ -175,10 +175,12 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/IBAnimatable/IBAnimatable.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Koloda/Koloda.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/pop/pop.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/IBAnimatable/IBAnimatable.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Koloda/Koloda.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/pop/pop.framework"
 fi
