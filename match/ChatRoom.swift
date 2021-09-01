0ae2615417bdc707decf819b22868ffa1467195e
樋口裕貴 <sai102343@gmail.com>
2021-09-01 22:06:47 +0900

    テーブルビューセル追加

--- match/ChatRoom.swift
+++ match/ChatRoom.swift
@@ -5,8 +5,23 @@
 //  Created by 樋口裕貴 on 2021/08/31.
 //
 
-import UIKit
+import Foundation
+import Firebase
 
-class ChatRoom: NSObject {
-
+class ChatRoom {
+    
+    let latestMessageId:String
+    let members:[String]
+    let createdAt: Timestamp
+    
+    var latestMessage: Message?
+    var documentId: String?
+    var partnerUser: User?
+    
+    init(dic: [String: Any]) {
+        self.latestMessageId = dic["latestMessageId"] as? String ?? ""
+        self.members = dic["memebers"] as? [String] ?? [String]()
+        self.createdAt = dic["createdAt"] as? Timestamp ?? Timestamp()
+    }
+    
 }
--- match/Message.swift
+++ match/Message.swift
@@ -5,8 +5,23 @@
 //  Created by 樋口裕貴 on 2021/08/31.
 //
 
-import UIKit
+import Foundation
+import Firebase
 
-class Message: NSObject {
-
+class Message {
+    
+    let name: String
+    let message: String
+    let uid: String
+    let createdAt: Timestamp
+    
+    var partnerUser: User?
+    
+    init(dic: [String: Any]) {
+        self.name = dic["name"] as? String ?? ""
+        self.message = dic["message"] as? String ?? ""
+        self.uid = dic["uid"] as? String ?? ""
+        self.createdAt = dic["createdAt"] as? Timestamp ?? Timestamp()
+    }
+    
 }
