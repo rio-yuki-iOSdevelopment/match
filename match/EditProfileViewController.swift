51f757b0954435452671fac3691d8e813926cf16
samrio_1122 <5260a.rio@gmail.com>
2021-09-08 21:46:28 +0900

    エウレカユーザーデフォルト

--- match/EditProfileViewController.swift
+++ match/EditProfileViewController.swift
@@ -19,55 +19,60 @@ class EditProfileViewController: FormViewController {
                 row.title = "ニックネーム"
                 row.placeholder = "ニックネームを入力"
             }.onChange{row in
-            //            firebaseに該当する行の値を入れる
-                
-                self.saveUserInfo()
-                        }
+                self.userDefault.setValue(row.value, forKey: "displayName")
+            }
             +++ Section("自己紹介")
             <<< TextAreaRow("自己紹介"){ row in
                 //                row.title = "自己紹介"
                 row.placeholder = "こんにちは。"
-            } //.onChange{row in
-            //                self.userDefault.setValue(row.value, forKey: "Memo")
-            //            }
+            } .onChange{row in
+                self.userDefault.setValue(row.value, forKey: "selfIntroduction")
+            }
             +++ Section("興味")
             <<< TextRow("興味"){ row in
-                //                row.title = "自己紹介"
+                //  row.title = "自己紹介"
                 row.placeholder = "ヒップホップ,ジム"
+            }.onChange{row in
+                self.userDefault.setValue(row.value, forKey: "selfIntroduction")
             }
+            
             +++ Section("仕事")
             <<< TextRow("仕事"){ row in
-                //                row.title = "自己紹介"
+                //  row.title = "自己紹介"
                 row.placeholder = "仕事を追加"
+            }.onChange{row in
+                self.userDefault.setValue(row.value, forKey: "work")
+                
             }
+            
+            
             +++ Section("")
             <<< ButtonRow("フォーム") {row in
+                
                 row.title = "完了"
+                
+                row.onCellSelection{[unowned self] ButtonCellOf, row in
+                    
+                    // ユーザー取得
+                    guard let user = Auth.auth().currentUser else {
+                        // サインインしていない場合の処理をするなど
+                        return
+                    }
+                    let db = Firestore.firestore()
+                    //        setData いけたけど既存データが消えたあああ
+                    db.collection("users").document(user.uid).setData([
+                        "displayName": "",
+                        "selfIntroduction": "",
+                        "hobby": "",
+                        "work": "",
+                    ]) { error in
+                        if let error = error {
+                            // エラー処理
+                            print(error)
+                        }
+                    }
+                    self.userDefault.removeAll()
+                }
             }
-        saveUserInfo()
-    }
-    
-    
-     func saveUserInfo() {
-        //        ユーザー取得
-        guard let user = Auth.auth().currentUser else {
-            // サインインしていない場合の処理をするなど
-            return
-        }
-        
-        let db = Firestore.firestore()
-//        setData いけたけど既存データが消えたあああ
-        db.collection("users").document(user.uid).setData([
-            "displayName": "aa",
-//            "introduction": "aa",
-//            "hobby": "",
-//            "work": "",
-        ]) { error in
-            if let error = error {
-                // エラー処理
-                print(error)
-            }
-          
-        }
     }
 }
